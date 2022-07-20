17b8e6eae04ccc3110893a28d880421f44b37955
Eric Charles <badass@stanford.edu>
2022-07-20 11:12:45 -0700

    added docstrings

--- piff_syst_plots/core.py
+++ piff_syst_plots/core.py
@@ -1,29 +1,39 @@
 """Core classes for piff_syst_plots"""
 
+from __future__ import annotations
+
 import os
-from typing import Any
-#import yaml
+from typing import Any, Optional
+
+# import yaml
 
 from collections import OrderedDict
 from dataclasses import dataclass
 import fitsio
 
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.figure import Figure
 
 
 class Plotter:
-    """Base class of object that make a single Figure"""
+    """Base class of object that make a single Figure
+
+    This class really just keeps track of the options that
+    you use to make the plot, via the `self._config` dictionary.
+
+    Derived classes should override the `_make_plot(self, cat)`
+    function to make and return a `matplotlib.figure.Figure`
+    """
 
     plotters = OrderedDict()
 
     default_config = {}
 
     def __init_subclass__(cls, **kwargs):
-        """
-        Python 3.6+ provides a facility to automatically
+        """Python 3.6+ provides a facility to automatically
         call a method (this one) whenever a new subclass
         is defined.  In this case we use that feature to keep
-        track of all available pipeline stages, each of which is
+        track of all available plots, each of which is
         defined by a class.
 
         """
@@ -31,33 +41,49 @@ class Plotter:
         cls.plotters[cls.__name__] = cls
 
     def __init__(self, **kwargs):
+        """Constructor, just copy kwargs to self._config"""
         self._config = self.default_config.copy()
         self._config.update(**kwargs)
 
     @property
-    def config(self):
+    def config(self) -> dict[str, Any]:
         """Configuration used to make plots"""
         return self._config
 
-    def __call__(self, cat, **kwargs):
+    def __call__(self, cat, **kwargs) -> Figure:
         """Main function, makes figure"""
         self._config.update(**kwargs)
         return self._make_plot(cat)
 
-    def _make_plot(self, cat):
+    def _make_plot(self, cat) -> Figure:
         """This function need to be implemented by the sub-class"""
         raise NotImplementedError()
 
 
 @dataclass
 class PlotConfig:
-    """Simple class to carry around the configuration we use to make a plot"""
+    """Simple class to carry around the configuration we use to make a plot
+
+    This includes:
+
+    Parameters
+    ----------
+    plotname : str
+        The name of the plot, used to refer to it, and also used to make
+        the output file name
+
+    classname : str
+        The name of the python class that will actually make the plot
+
+    plot_config : dict[str, Any]
+        The configuration options for making the plot
+    """
 
     plotname: str
     classname: str
     plot_config: dict[str, Any]
 
-    def to_dict(self):
+    def to_dict(self) -> dict[str, Any]:
         """Return the PlotConfig as a dict"""
         return dict(
             plotname=self.plotname,
@@ -66,7 +92,7 @@ class PlotConfig:
         )
 
     @classmethod
-    def from_dict(cls, the_dict):
+    def from_dict(cls, the_dict) -> PlotConfig:
         """Create a PlotConfig object from a dict"""
         return cls(
             plotname=the_dict["plotname"],
@@ -82,7 +108,7 @@ class PlotConfig:
         return plotter_class(**self.plot_config)
 
 
-def add_pdf_metatada(pdffig, metadata):
+def add_pdf_metatada(pdffig: PdfPages, metadata: dict[str, str]) -> None:
     """Add provenance metadata to a pdf file"""
     pdf_metadata = pdffig.infodict()
     for key, value in metadata.items():
@@ -90,14 +116,31 @@ def add_pdf_metatada(pdffig, metadata):
 
 
 class PlotCollection:
-    """A collection of plots we are making"""
+    """A collection of plots we are making
+
+    Individual plots are refered to by `plot_name`, which
+    is used as the key to all the internal dictionaries.
+    """
 
     @staticmethod
-    def get_version():
+    def get_version() -> str:
         """Return the version of the code used to make the plot"""
         return "v1"
 
-    def __init__(self, data_url, output_dir, config=None):
+    def __init__(self, data_url: str, output_dir: str, config: Optional[str] = None):
+        """Constructor
+
+        Parameters
+        ----------
+        data_url : str
+            The path to the data being used to make these plots
+
+        output_dir: str
+            The path to where we want to store these plots
+
+        config : str
+            The path to a yaml file we can use to (re)-configure the plots
+        """
         self._data_url = data_url
         self._catalog = fitsio.FITS(self._data_url)[1]
         self._catalog.upper = True
@@ -108,23 +151,23 @@ class PlotCollection:
         self._plotter_dict = OrderedDict()
         self.read_config(config)
 
-    def read_config(self, config_url):
+    def read_config(self, config_url: str) -> None:
         """Get a list of PlotInfo objects from a yaml file"""
         if config_url is None:
             return
-        with open(config_url, 'rt', encoding='utf-8') as config_file:
-            #config_data = yaml.load(config_file)
+        with open(config_url, "rt", encoding="utf-8") as config_file:
+            # config_data = yaml.load(config_file)
             config_data = []
         for config in config_data:
             plot_info = PlotConfig.from_dict(config)
             self._plot_info_dict[plot_info.plotname] = plot_info
 
-    def add_default_plotters(self):
+    def add_default_plotters(self) -> None:
         """Add a bunch of plotters"""
         for key in Plotter.plotters.keys():
             self._plot_info_dict[key] = PlotConfig(key, key, {})
-            
-    def make_plots(self, plot_names):
+
+    def make_plots(self, plot_names: str) -> None:
         """Make all the plots in the list"""
         if not plot_names:
             plot_names = self._plot_info_dict.keys()
@@ -134,9 +177,9 @@ class PlotCollection:
             self._plotter_dict[plot_info.plotname] = the_plotter
             self._fig_dict[plot_info.plotname] = the_plotter(self._catalog)
 
-    def _save_annotated(self, fig, fig_name, save_name):
+    def _save_annotated(self, fig: Figure, plot_name: str, save_name: str) -> None:
         """Save the fig in a PDF with provencance metadata"""
-        plot_info = self._plot_info_dict[fig_name]
+        plot_info = self._plot_info_dict[plot_name]
         metadata = dict(
             version=self.get_version(),
             data_url=self._data_url,
@@ -147,39 +190,56 @@ class PlotCollection:
         with PdfPages(save_name) as pdf:
             add_pdf_metatada(pdf, metadata)
 
-    def show_fig(self, fig_name):
+    def show_fig(self, plot_name: str) -> None:
         """Show a single figure to the screen"""
-        fig = self._fig_dict[fig_name]
+        fig = self._fig_dict[plot_name]
         fig.show()
 
-    def show_figs(self, fig_name_list=None):
+    def show_figs(self, plot_name_list: Optional[list[str]] = None):
         """Show all the figures to the screen"""
-        if not fig_name_list:
-            fig_name_list = self._fig_dict.keys()
-        for fig_name in fig_name_list:
-            self.show_fig(fig_name)
+        if not plot_name_list:
+            plot_name_list = self._fig_dict.keys()
+        for plot_name in plot_name_list:
+            self.show_fig(plot_name)
 
-    def save_figs(self, fig_name_list=None, file_type="pdf", annotated=False):
-        """Save all the figure in the list"""
-        if not fig_name_list:
-            fig_name_list = self._fig_dict.keys()
+    def save_figs(
+        self,
+        plot_name_list: Optional[list[str]] = None,
+        file_type: str = "pdf",
+        annotated: bool = False,
+    ) -> None:
+        """Save all the figures in the list"""
+        if not plot_name_list:
+            plot_name_list = self._fig_dict.keys()
 
         try:
             os.makedirs(self._output_dir)
         except OSError:
             pass
-        for fig_name in fig_name_list:
-            fig = self._fig_dict[fig_name]
+        for plot_name in plot_name_list:
+            fig = self._fig_dict[plot_name]
             if fig is None:
                 return
-            save_name = os.path.join(self._output_dir, f"{fig_name}.{file_type}")
+            save_name = os.path.join(self._output_dir, f"{plot_name}.{file_type}")
             if file_type == "pdf" and annotated:
-                self._save_annotated(fig, fig_name, save_name)
+                self._save_annotated(fig, plot_name, save_name)
             else:
                 fig.savefig(save_name)
 
-    def add_plot(self, plot_name, plotter_class, **kwargs):
-        """Add a plot to this collection"""
+    def add_plot(self, plot_name: str, plotter_class: type, **kwargs) -> Figure:
+        """Add a plot to this collection
+
+        Parameters
+        ----------
+        plot_name : str
+            Name we will use for the plot
+            this is also used to make the output file name
+
+        plotter_class : type
+            Class actually used to make the plot
+
+        Keywords are passed to the plotter_class to configure it
+        """
         plot_info = PlotConfig(plot_name, plotter_class.__name__, kwargs)
         the_plotter = plot_info.build_plotter()
         self._plot_info_dict[plot_info.plotname] = plot_info
